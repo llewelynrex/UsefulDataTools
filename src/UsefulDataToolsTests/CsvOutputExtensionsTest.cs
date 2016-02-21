decca90034467b3e9930ee4b59130aee74fb0be3
Llewelyn Rex <llewelyn.rex@outlook.com>
2016-02-21 21:48:05 +0100

    Started adding CSV Test

---
+++ src/UsefulDataToolsTests/CsvOutputExtensionsTest.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UsefulDataTools;
+
+namespace UsefulDataToolsTests
+{
+    [TestClass]
+    public class CsvOutputExtensionsTest
+    {
+        public List<TestDemoClass> TestDemoClasses { get; set; }
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            TestDemoClasses = new List<TestDemoClass>();
+
+            var today = DateTime.Today;
+            var utc = DateTime.UtcNow;
+
+            var testDemoClass1 = new TestDemoClass
+                                 {
+                                     BooleanField = true,
+                                     BooleanProperty = false,
+                                     ByteField = 1,
+                                     ByteProperty = 0,
+                                     CharacterField = 'a',
+                                     CharacterProperty = 'A',
+                                     DateTimeField = today,
+                                     DateTimeProperty = utc,
+                                     DecimalField = 1M,
+                                     DecimalProperty = 0M,
+                                     DoubleField = 1,
+                                     DoubleProperty = 0,
+                                     FloatField = 0,
+                                     FloatProperty = 0,
+                                     IntField = 1,
+                                     IntProperty = 0,
+                                     LongField = 1,
+                                     LongProperty = 0,
+                                     NullableBooleanField = false,
+                                     NullableBooleanProperty = true,
+                                     NullableByteField = 0,
+                                     NullableByteProperty = 1,
+                                     NullableCharacterField = 'A',
+                                     NullableCharacterProperty = 'a',
+                                     NullableDateTimeField = utc,
+                                     NullableDateTimeProperty = today,
+                                     NullableDecimalField = 0M,
+                                     NullableDecimalProperty = 1M,
+                                     NullableDoubleField = 0,
+                                     NullableDoubleProperty = 1,
+                                     NullableFloatField = 0,
+                                     NullableFloatProperty = 1,
+                                     NullableIntField = 0,
+                                     NullableIntProperty = 1,
+                                     NullableLongField = 0,
+                                     NullableLongProperty = 1,
+                                     NullableSByteField = 0,
+                                     NullableSByteProperty = 1,
+                                     NullableShortField = 0,
+                                     NullableShortProperty = 1,
+                                     NullableUIntField = 0,
+                                     NullableUIntProperty = 1,
+                                     NullableULongField = 0,
+                                     NullableULongProperty = 1,
+                                     NullableUShortField = 0,
+                                     NullableUShortProperty = 1,
+                                     SByteField = 1,
+                                     SByteProperty = 0,
+                                     ShortField = 1,
+                                     ShortProperty = 0,
+                                     StringField = "Foo Bar",
+                                     StringProperty = "Bar Foo",
+                                     UIntField = 1,
+                                     UIntProperty = 0,
+                                     ULongField = 1,
+                                     ULongProperty = 0,
+                                     UShortField = 1,
+                                     UShortProperty = 0
+                                 };
+
+            var testDemoClass2 = new TestDemoClass
+                                 {
+                                     BooleanField = true,
+                                     BooleanProperty = false,
+                                     ByteField = 1,
+                                     ByteProperty = 0,
+                                     CharacterField = 'a',
+                                     CharacterProperty = 'A',
+                                     DateTimeField = today,
+                                     DateTimeProperty = utc,
+                                     DecimalField = 1M,
+                                     DecimalProperty = 0M,
+                                     DoubleField = 1,
+                                     DoubleProperty = 0,
+                                     FloatField = 0,
+                                     FloatProperty = 0,
+                                     IntField = 1,
+                                     IntProperty = 0,
+                                     LongField = 1,
+                                     LongProperty = 0,
+                                     NullableBooleanField = false,
+                                     NullableBooleanProperty = true,
+                                     NullableByteField = 0,
+                                     NullableByteProperty = 1,
+                                     NullableCharacterField = 'A',
+                                     NullableCharacterProperty = 'a',
+                                     NullableDateTimeField = utc,
+                                     NullableDateTimeProperty = today,
+                                     NullableDecimalField = 0M,
+                                     NullableDecimalProperty = 1M,
+                                     NullableDoubleField = 0,
+                                     NullableDoubleProperty = 1,
+                                     NullableFloatField = 0,
+                                     NullableFloatProperty = 1,
+                                     NullableIntField = 0,
+                                     NullableIntProperty = 1,
+                                     NullableLongField = 0,
+                                     NullableLongProperty = 1,
+                                     NullableSByteField = 0,
+                                     NullableSByteProperty = 1,
+                                     NullableShortField = 0,
+                                     NullableShortProperty = 1,
+                                     NullableUIntField = 0,
+                                     NullableUIntProperty = 1,
+                                     NullableULongField = 0,
+                                     NullableULongProperty = 1,
+                                     NullableUShortField = 0,
+                                     NullableUShortProperty = 1,
+                                     SByteField = 1,
+                                     SByteProperty = 0,
+                                     ShortField = 1,
+                                     ShortProperty = 0,
+                                     StringField = "Foo Bar",
+                                     StringProperty = "Bar Foo",
+                                     UIntField = 1,
+                                     UIntProperty = 0,
+                                     ULongField = 1,
+                                     ULongProperty = 0,
+                                     UShortField = 1,
+                                     UShortProperty = 0
+                                 };
+
+            TestDemoClasses.Add(testDemoClass1);
+            TestDemoClasses.Add(testDemoClass2);
+        }
+
+        [TestMethod]
+        public void TestCsvOutput()
+        {
+            var today = DateTime.Today;
+            var utc = DateTime.UtcNow;
+
+            var csv = TestDemoClasses.ToCsv();
+        }
+    }
+}
---
+++ src/UsefulDataToolsTests/TestDemoClass.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace UsefulDataToolsTests
+{
+    public class TestDemoClass
+    {
+        public byte ByteProperty { get; set; }
+        public sbyte SByteProperty { get; set; }
+        public int IntProperty { get; set; }
+        public uint UIntProperty { get; set; }
+        public short ShortProperty { get; set; }
+        public ushort UShortProperty { get; set; }
+        public long LongProperty { get; set; }
+        public ulong ULongProperty { get; set; }
+        public float FloatProperty { get; set; }
+        public double DoubleProperty { get; set; }
+        public decimal DecimalProperty { get; set; }
+        public bool BooleanProperty { get; set; }
+        public char CharacterProperty { get; set; }
+        public DateTime DateTimeProperty { get; set; }
+        public byte? NullableByteProperty { get; set; }
+        public sbyte? NullableSByteProperty { get; set; }
+        public int? NullableIntProperty { get; set; }
+        public uint? NullableUIntProperty { get; set; }
+        public short? NullableShortProperty { get; set; }
+        public ushort? NullableUShortProperty { get; set; }
+        public long? NullableLongProperty { get; set; }
+        public ulong? NullableULongProperty { get; set; }
+        public float? NullableFloatProperty { get; set; }
+        public double? NullableDoubleProperty { get; set; }
+        public decimal? NullableDecimalProperty { get; set; }
+        public bool? NullableBooleanProperty { get; set; }
+        public char? NullableCharacterProperty { get; set; }
+        public DateTime? NullableDateTimeProperty { get; set; }
+        public string StringProperty { get; set; }
+
+        public byte ByteField;
+        public sbyte SByteField;
+        public int IntField;
+        public uint UIntField;
+        public short ShortField;
+        public ushort UShortField;
+        public long LongField;
+        public ulong ULongField;
+        public float FloatField;
+        public double DoubleField;
+        public decimal DecimalField;
+        public bool BooleanField;
+        public char CharacterField;
+        public DateTime DateTimeField;
+        public byte? NullableByteField;
+        public sbyte? NullableSByteField;
+        public int? NullableIntField;
+        public uint? NullableUIntField;
+        public short? NullableShortField;
+        public ushort? NullableUShortField;
+        public long? NullableLongField;
+        public ulong? NullableULongField;
+        public float? NullableFloatField;
+        public double? NullableDoubleField;
+        public decimal? NullableDecimalField;
+        public bool? NullableBooleanField;
+        public char? NullableCharacterField;
+        public DateTime? NullableDateTimeField;
+        public string StringField;
+    }
+}
