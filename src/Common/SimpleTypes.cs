fb09bb3f01c82694b68b2980d70e06e280af64e8
Llewelyn Rex <llewelyn.rex@outlook.com>
2016-02-26 18:11:19 +0100

    Documented SimpleTypes static class functions.

--- src/Common/SimpleTypes.cs
+++ src/Common/SimpleTypes.cs
@@ -28,6 +28,12 @@ namespace UsefulDataTools
                                    t.GetTypeInfo().BaseType == typeof (Enum);
         }
 
+        /// <summary>
+        /// Evaluates whether <see cref="T"/> is a simple type.
+        /// </summary>
+        /// <typeparam name="T">T is only valid for System.Type.</typeparam>
+        /// <param name="type">The type object which is to be evaluated.</param>
+        /// <returns>bool</returns>
         public static bool IsSimpleType<T>(this T type)
             where T : Type
         {
@@ -36,6 +42,11 @@ namespace UsefulDataTools
             return SimpleTypesPredicate(type);
         }
 
+        /// <summary>
+        /// Evaluates to see whether <see cref="type"/> is a simple type.
+        /// </summary>
+        /// <param name="type">The type object which is to be evaluated.</param>
+        /// <returns>bool</returns>
         public static bool IsSimpleType(Type type)
         {
             return type.IsSimpleType();
