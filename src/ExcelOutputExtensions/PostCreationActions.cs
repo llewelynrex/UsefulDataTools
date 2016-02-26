caeeaf02779455bd8f848f8927fe70ef15f6452c
Llewelyn Rex <llewelyn.rex@outlook.com>
2016-02-26 18:41:24 +0100

    Documented the enum PostCreationActions

--- src/ExcelOutputExtensions/PostCreationActions.cs
+++ src/ExcelOutputExtensions/PostCreationActions.cs
@@ -1,9 +1,21 @@
 ﻿namespace UsefulDataTools
 {
+    /// <summary>
+    /// Enumerator of actions to perform after the Excel file has been generated.
+    /// </summary>
     public enum PostCreationActions
     {
+        /// <summary>
+        /// Open Excel and display the generated table.
+        /// </summary>
         Open,
+        /// <summary>
+        /// Save the file to the specified location, then open Excel and display the generated table.
+        /// </summary>
         SaveAndView,
+        /// <summary>
+        /// Save the file to the specified location
+        /// </summary>
         SaveAndClose
     }
 }
