f39316f75c385a159c83485ea1c149b9dc44d166
Llewelyn Rex <llewelyn.rex@outlook.com>
2016-03-04 22:15:19 +0100

    Added trimming functionality to the CsvOutputExtensions.

--- src/CsvOutputExtensions45/CsvOutputExtensions.cs
+++ src/CsvOutputExtensions45/CsvOutputExtensions.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Reflection;
 using System.Text;
 
 namespace UsefulDataTools
@@ -9,89 +8,183 @@ namespace UsefulDataTools
     public static class CsvOutputExtensions
     {
         /// <summary>
-        /// Returns a csv string using all of the public properties and public fields from each 
-        /// item of type <see cref="T"/> in the <see cref="IEnumerable{T}"/> collection using the default separator.
+        ///     Returns a csv string using all of the public properties and public fields from each
+        ///     item of type <see cref="T" /> in the <see cref="IEnumerable{T}" /> collection using the default separator.
         /// </summary>
         /// <typeparam name="T">Any type T.</typeparam>
-        /// <param name="input">An <see cref="IEnumerable{T}"/>.</param>
-        /// <returns><see cref="string"/></returns>
+        /// <param name="input">An <see cref="IEnumerable{T}" />.</param>
+        /// <returns>
+        ///     <see cref="string" />
+        /// </returns>
         public static string ToCsv<T>(this IEnumerable<T> input)
         {
-            return input.ToCsv(',',null);
+            return input.ToCsv(',', null, false);
         }
 
         /// <summary>
-        /// Returns a csv string using all of the public properties and public fields from each 
-        /// item of Type T in the IEnumerable collection using the specified <see cref="separator"/> 
-        /// character.
+        ///     Returns a csv string using all of the public properties and public fields from each
+        ///     item of type <see cref="T" /> in the <see cref="IEnumerable{T}" /> collection using the default separator.
         /// </summary>
         /// <typeparam name="T">Any type T.</typeparam>
-        /// <param name="input">An <see cref="IEnumerable{T}"/>.</param>
+        /// <param name="input">An <see cref="IEnumerable{T}" />.</param>
+        /// <param name="trim">A parameter which indicates whether the strings for each element should be trimmed or not.</param>
+        /// <returns>
+        ///     <see cref="string" />
+        /// </returns>
+        public static string ToCsv<T>(this IEnumerable<T> input, bool trim)
+        {
+            return input.ToCsv(',', null, trim);
+        }
+
+        /// <summary>
+        ///     Returns a csv string using all of the public properties and public fields from each
+        ///     item of Type T in the IEnumerable collection using the specified <see cref="separator" />
+        ///     character.
+        /// </summary>
+        /// <typeparam name="T">Any type T.</typeparam>
+        /// <param name="input">An <see cref="IEnumerable{T}" />.</param>
         /// <param name="separator">A separator to use for the csv output.</param>
-        /// <returns><see cref="string"/></returns>
+        /// <returns>
+        ///     <see cref="string" />
+        /// </returns>
         public static string ToCsv<T>(this IEnumerable<T> input, char separator)
         {
-            return input.ToCsv(separator, null);
+            return input.ToCsv(separator, null, false);
         }
 
         /// <summary>
-        /// Returns a csv string using all of the public properties and public fields from each 
-        /// item of Type T in the IEnumerable collection using the default separator ',' and saves 
-        /// the generated string to <see cref="path"/>.
+        ///     Returns a csv string using all of the public properties and public fields from each
+        ///     item of Type T in the IEnumerable collection using the specified <see cref="separator" />
+        ///     character.
         /// </summary>
         /// <typeparam name="T">Any type T.</typeparam>
-        /// <param name="input">An <see cref="IEnumerable{T}"/>.</param>
+        /// <param name="input">An <see cref="IEnumerable{T}" />.</param>
+        /// <param name="separator">A separator to use for the csv output.</param>
+        /// <param name="trim">A parameter which indicates whether the strings for each element should be trimmed or not.</param>
+        /// <returns>
+        ///     <see cref="string" />
+        /// </returns>
+        public static string ToCsv<T>(this IEnumerable<T> input, char separator, bool trim)
+        {
+            return input.ToCsv(separator, null, trim);
+        }
+
+        /// <summary>
+        ///     Returns a csv string using all of the public properties and public fields from each
+        ///     item of Type T in the IEnumerable collection using the default separator ',' and saves
+        ///     the generated string to <see cref="path" />.
+        /// </summary>
+        /// <typeparam name="T">Any type T.</typeparam>
+        /// <param name="input">An <see cref="IEnumerable{T}" />.</param>
         /// <param name="path">A path string which is used to automatically write the generated csv to a file.</param>
-        /// <returns><see cref="string"/></returns>
+        /// <returns>
+        ///     <see cref="string" />
+        /// </returns>
         public static string ToCsv<T>(this IEnumerable<T> input, string path)
         {
-            return input.ToCsv(',', path);
+            return input.ToCsv(',', path, false);
         }
 
         /// <summary>
-        /// Returns a csv string using all of the public properties and public fields from each 
-        /// item of Type T in the IEnumerable collection using the specified <see cref="separator"/> 
-        /// character and saves the generated string to <see cref="path"/>.
+        ///     Returns a csv string using all of the public properties and public fields from each
+        ///     item of Type T in the IEnumerable collection using the default separator ',' and saves
+        ///     the generated string to <see cref="path" />.
         /// </summary>
         /// <typeparam name="T">Any type T.</typeparam>
-        /// <param name="input">An <see cref="IEnumerable{T}"/>.</param>
+        /// <param name="input">An <see cref="IEnumerable{T}" />.</param>
+        /// <param name="path">A path string which is used to automatically write the generated csv to a file.</param>
+        /// <param name="trim">A parameter which indicates whether the strings for each element should be trimmed or not.</param>
+        /// <returns>
+        ///     <see cref="string" />
+        /// </returns>
+        public static string ToCsv<T>(this IEnumerable<T> input, string path, bool trim)
+        {
+            return input.ToCsv(',', path, trim);
+        }
+
+        /// <summary>
+        ///     Returns a csv string using all of the public properties and public fields from each
+        ///     item of Type T in the IEnumerable collection using the specified <see cref="separator" />
+        ///     character and saves the generated string to <see cref="path" />.
+        /// </summary>
+        /// <typeparam name="T">Any type T.</typeparam>
+        /// <param name="input">An <see cref="IEnumerable{T}" />.</param>
         /// <param name="separator">A separator to use for the csv output.</param>
         /// <param name="path">A path string which is used to automatically write the generated csv to a file.</param>
-        /// <returns><see cref="string"/></returns>
-        public static string ToCsv<T>(this IEnumerable<T> input, char separator, string path)
+        /// <param name="trim">A parameter which indicates whether the strings for each element should be trimmed or not.</param>
+        /// <returns>
+        ///     <see cref="string" />
+        /// </returns>
+        public static string ToCsv<T>(this IEnumerable<T> input, char separator, string path, bool trim)
         {
             var type = typeof (T);
             string output;
 
-            if (type.IsSimpleType())
-                output = string.Join(separator.ToString(), input);
-
-            else
+            if (trim)
             {
-                var properties = type.GetProperties();
-                var fields = type.GetFields();
-                var stringBuilder = new StringBuilder();
+                if (type.IsSimpleType())
+                    output = string.Join(separator.ToTrimmedString(), input);
 
-                stringBuilder.AppendLine(
-                                         string.Concat(
-                                                       string.Join(separator.ToString(), properties.Where(p => p.PropertyType.IsSimpleType()).Select(p => p.Name).ToArray()),
-                                                       fields.Any(f => f.FieldType.IsSimpleType()) ? separator.ToString() : string.Empty,
-                                                       string.Join(separator.ToString(), fields.Where(f => f.FieldType.IsSimpleType()).Select(f => f.Name).ToArray())
-                                             )
-                    );
-
-                foreach (var element in input)
+                else
                 {
+                    var properties = type.GetProperties();
+                    var fields = type.GetFields();
+                    var stringBuilder = new StringBuilder();
+
                     stringBuilder.AppendLine(
                                              string.Concat(
-                                                           string.Join(separator.ToString(), properties.Where(p => p.PropertyType.IsSimpleType()).Select(p => p.GetValue(element)).ToArray()),
+                                                           string.Join(separator.ToString(), properties.Where(p => p.PropertyType.IsSimpleType()).Select(p => p.Name.ToTrimmedString()).ToArray()),
                                                            fields.Any(f => f.FieldType.IsSimpleType()) ? separator.ToString() : string.Empty,
-                                                           string.Join(separator.ToString(), fields.Where(f => f.FieldType.IsSimpleType()).Select(f => f.GetValue(element)).ToArray())
+                                                           string.Join(separator.ToString(), fields.Where(f => f.FieldType.IsSimpleType()).Select(f => f.Name.ToTrimmedString()).ToArray())
                                                  )
                         );
-                }
 
-                output = stringBuilder.ToString();
+                    foreach (var element in input)
+                    {
+                        stringBuilder.AppendLine(
+                                                 string.Concat(
+                                                               string.Join(separator.ToString(), properties.Where(p => p.PropertyType.IsSimpleType()).Select(p => p.GetValue(element).ToTrimmedString()).ToArray()),
+                                                               fields.Any(f => f.FieldType.IsSimpleType()) ? separator.ToString() : string.Empty,
+                                                               string.Join(separator.ToString(), fields.Where(f => f.FieldType.IsSimpleType()).Select(f => f.GetValue(element).ToTrimmedString()).ToArray())
+                                                     )
+                            );
+                    }
+
+                    output = stringBuilder.ToString();
+                }
+            }
+            else
+            {
+                if (type.IsSimpleType())
+                    output = string.Join(separator.ToString(), input);
+
+                else
+                {
+                    var properties = type.GetProperties();
+                    var fields = type.GetFields();
+                    var stringBuilder = new StringBuilder();
+
+                    stringBuilder.AppendLine(
+                                             string.Concat(
+                                                           string.Join(separator.ToString(), properties.Where(p => p.PropertyType.IsSimpleType()).Select(p => p.Name).ToArray()),
+                                                           fields.Any(f => f.FieldType.IsSimpleType()) ? separator.ToString() : string.Empty,
+                                                           string.Join(separator.ToString(), fields.Where(f => f.FieldType.IsSimpleType()).Select(f => f.Name).ToArray())
+                                                 )
+                        );
+
+                    foreach (var element in input)
+                    {
+                        stringBuilder.AppendLine(
+                                                 string.Concat(
+                                                               string.Join(separator.ToString(), properties.Where(p => p.PropertyType.IsSimpleType()).Select(p => p.GetValue(element)).ToArray()),
+                                                               fields.Any(f => f.FieldType.IsSimpleType()) ? separator.ToString() : string.Empty,
+                                                               string.Join(separator.ToString(), fields.Where(f => f.FieldType.IsSimpleType()).Select(f => f.GetValue(element)).ToArray())
+                                                     )
+                            );
+                    }
+
+                    output = stringBuilder.ToString();
+                }
             }
 
             if (!string.IsNullOrEmpty(path))
